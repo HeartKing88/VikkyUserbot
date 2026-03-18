baf38a2750715403f8770588380120fd0a0c0840
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2026-03-18 08:45:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+LOGGER_ID = int(os.getenv("LOGGER_ID"))
+
+OWNER_ID = None
