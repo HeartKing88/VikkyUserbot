01c11725b177efe9823728ea3c2048bb0f73e335
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2026-03-18 09:08:19 +0530

    Update pm_permit.py

--- vikky/modules/pm_permit.py
+++ vikky/modules/pm_permit.py
@@ -1,24 +1,81 @@
 from pyrogram import filters
-from vikky import assistant
+from vikky import vikky
+from vikky.utils.filters import owner_or_sudo
 import asyncio
 
 approved = set()
 warn = {}
+last_msg = {}
 
-@assistant.on_message(filters.private & ~filters.me)
-async def pm(client, message):
-    user = message.from_user.id
+LIMIT = 3
 
-    if user in approved:
+# 🖼️ apni photo ka link ya file_id daalna
+PM_PIC = "https://graph.org/file/your_image.jpg"
+
+
+# 📝 Message templates
+MSG_PERMIT = """
+<b>PM_SECURITY VIKKY-USERBOT</b>
+
+{}
+
+━━━━━━━━━━━━━━━━━━━━━━━
+⍟ You have <b>{}/{}</b> warning!!!
+"""
+
+DEFAULT = """
+<b>WELCOME....</b>
+
+Hi, this is the keeper of private messages.  
+Don't spam or you'll be blocked.
+
+⏳ Wait until my master receives your message.
+"""
+
+
+# 🔐 PM Permit
+@vikky.on_message(filters.private & filters.incoming & ~filters.me)
+async def pm_permit(client, message):
+    if not message.from_user:
         return
 
-    warn[user] = warn.get(user, 0) + 1
+    user = message.from_user
 
-    if warn[user] == 1:
-        await message.reply("⚠️ Wait for approval")
-    elif warn[user] == 2:
-        await message.reply("⚠️ Last warning")
-    else:
-        await message.reply("❌ Blocking...")
-        await asyncio.sleep(3)
-        await client.block_user(user)
+    if user.is_bot or user.is_self or user.is_contact:
+        return
+
+    if user.id in approved:
+        return
+
+    # warning count
+    warn[user.id] = warn.get(user.id, 0) + 1
+
+    # delete old msg
+    if user.id in last_msg:
+        try:
+            await last_msg[user.id].delete()
+        except:
+            pass
+
+    # spam detect
+    if warn[user.id] > LIMIT:
+        await message.reply("🚫 Spammer detected! Blocking...")
+        await client.block_user(user.id)
+        warn[user.id] = 0
+        return
+
+    # final text
+    text = MSG_PERMIT.format(
+        DEFAULT,
+        warn[user.id],
+        LIMIT
+    )
+
+    # 📸 send photo + caption
+    msg = await message.reply_photo(
+        PM_PIC,
+        caption=text,
+        parse_mode="html"
+    )
+
+    last_msg[user.id] = msg
