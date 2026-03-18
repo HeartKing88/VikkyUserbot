c2e4f1dc33d7b91727e0fbfbbc487a513841e342
Vikky yadav <146602351+HeartKing88@users.noreply.github.com>
2026-03-18 08:55:40 +0530

    Update __init__.py

--- vikky/__init__.py
+++ vikky/__init__.py
@@ -1,13 +1,15 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, STRING_SESSION, BOT_TOKEN
 
-assistant = Client(
+# 👤 Vikky (userbot)
+vikky = Client(
     "vikky_userbot",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=STRING_SESSION
 )
 
+# 🤖 Bot
 bot = Client(
     "vikky_bot",
     api_id=API_ID,
